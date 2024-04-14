c6887c075e1e7945ed55b921dc7d68213d8fb32f
enesozmus <ensozmus@gmail.com>
2024-04-14 14:26:17 +0300

    Initial Commit

---
+++ RollTheDice/RollTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceApp.swift
+//  RollTheDice
+//
+//  Created by enesozmus on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
